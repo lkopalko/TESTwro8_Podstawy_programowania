222e876b55c8b473273e4ad81612ff0f601a2e94
Lukasz <lkopalko@gmail.com>
2020-07-12 13:43:20 +0200

    Revert "tralalolo"
    This reverts commit da5c0bc6

--- My_Project/src/SecProg.java
+++
@@ -1,8 +0,0 @@
-public class SecProg {
-    public static void main(String[] args) {
-        assert args.length == 2 : "Wymagana ilosc args 2";
-        String ziom = args[0];
-        String nieziom = args[1];
-        System.out.println(ziom.concat(" ").concat(nieziom));
-    }
-}
