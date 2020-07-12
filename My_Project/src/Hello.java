09da41598503063d7ef706a7a4680f25ba230913
Lukasz <lkopalko@gmail.com>
2020-07-12 12:47:20 +0200

    Jakis pierwszy program

---
+++ My_Project/src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args){
+    String name = "Lukasz";
+    String surname = "Kopalko";
+    System.out.println(name + " " + surname);
+    System.out.println("FIN");
+    }
+
+}
